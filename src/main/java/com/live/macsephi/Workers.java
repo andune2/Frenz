80a392ef9862c47f3bcf04d61d02a416fdcd75ec
Mark Morgan <morganm.qc@gmail.com>
2012-09-15 16:34:45 -0700

    code refactor, no functional change

--- src/main/java/com/live/macsephi/Workers.java
+++ src/main/java/com/live/macsephi/Workers.java
@@ -8,175 +8,183 @@ import org.bukkit.entity.Player;
 
 import com.live.macsephi.Frenz;
 
+/** Andune 9/15/12 refactor/cleanup:
+ * 
+ *  - Eliminate private member variable "player", it's never saved between calls of onCommand(), so should be local to that method
+ *  - call command.getName() once and store it
+ *  - short-circuit Player check, instead of checking in each command
+ *  - rename "me" variable to "plugin" to be more descriptive
+ *  - change "this.plugin" to just "plugin" (reads better, same thing) 
+ *  - change logic to if/else ladders with a single return at the end
+ *
+ */
 public class Workers implements CommandExecutor {
-	private Player player;
-	private final Frenz me;
+	private final Frenz plugin;
 
-	public Workers(Frenz me) {
-		this.me = me;
+	public Workers(Frenz plugin) {
+		this.plugin = plugin;
 	}
-	public boolean onCommand(CommandSender sender, Command command,
-			String label, String[] args) {
-		if ((command.getName().equalsIgnoreCase("miner"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
-				if (this.me.hasPluginPermission(this.player, "miner")) {
-					if (this.me.Miner.contains(this.player)) {
-						this.me.unsetPotion(this.player, 3);
-						this.me.Miner.remove(this.player);
-						this.player.sendMessage(ChatColor.DARK_GRAY
-								+"Y"+ChatColor.GRAY+"ou have calmed your own mining enhancement at will.");
-						return true;
-					}
-					this.me.Miner.add(this.player);
-					this.me.setPotion(this.player, 3, 1800, 1);
-					this.player.sendMessage(ChatColor.AQUA
-							+"Y"+ChatColor.BLUE + "our arms feel refreshed and rejuvenated!");
-					this.player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"DEX"
-							+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
-							"1"+ChatColor.DARK_BLUE+"]");
-					return true;
-				}
-				this.player.sendMessage(ChatColor.DARK_RED
-						+"Y"+ChatColor.RED+"ou either need to join the Worker's Guild," +
-						" or earn a higher rank within it.");
-			}
-			return false;
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+	    Player player = null;
+	    if( !(sender instanceof Player) ) {
+	        sender.sendMessage("Only players may use this command");   // tell console to buzz off
+	        return false;
+	    }
+	    else {
+	        player = (Player) sender;
+	    }
+	    
+	    final String cmdName = command.getName();
+	    
+		if (cmdName.equalsIgnoreCase("miner")) {
+		    if (plugin.hasPluginPermission(player, "miner")) {
+		        if (plugin.Miner.contains(player)) {
+		            plugin.unsetPotion(player, 3);
+		            plugin.Miner.remove(player);
+		            player.sendMessage(ChatColor.DARK_GRAY
+		                    +"Y"+ChatColor.GRAY+"ou have calmed your own mining enhancement at will.");
+		        }
+		        else {
+		            plugin.Miner.add(player);
+		            plugin.setPotion(player, 3, 1800, 1);
+		            player.sendMessage(ChatColor.AQUA
+		                    +"Y"+ChatColor.BLUE + "our arms feel refreshed and rejuvenated!");
+		            player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"DEX"
+		                    +ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
+		                    "1"+ChatColor.DARK_BLUE+"]");
+		        }
+		    }
+		    else {
+		        player.sendMessage(ChatColor.DARK_RED
+		                +"Y"+ChatColor.RED+"ou either need to join the Worker's Guild," +
+		                " or earn a higher rank within it.");
+		    }
 		}
-		if ((command.getName().equalsIgnoreCase("betterminer"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
-				if (this.me.hasPluginPermission(this.player, "betterminer")) {
-					if (this.me.mBetter.contains(this.player)) {
-						this.me.unsetPotion(this.player, 3);
-						this.me.mBetter.remove(this.player);
-						this.player.sendMessage(ChatColor.DARK_GRAY
-								+"Y"+ChatColor.GRAY+"ou have calmed your own mining enhancement at will.");
-						return true;
-					}
-					this.me.mBetter.add(this.player);
-					this.me.setPotion(this.player, 3, 3000, 2);
-					this.player.sendMessage(ChatColor.AQUA
-							+"Y"+ChatColor.BLUE + "our arms suddenly feel more buff and tough!");
-					this.player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"DEX"
-							+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
-							"2"+ChatColor.DARK_BLUE+"]");
-					return true;
-				} 
-				this.player.sendMessage(ChatColor.DARK_RED
-						+"Y"+ChatColor.RED+"ou either need to join the Worker's Guild," +
-						" or earn a higher rank within it.");
-			}
-			return false;
+		else if (cmdName.equalsIgnoreCase("betterminer")) {
+		    if (plugin.hasPluginPermission(player, "betterminer")) {
+		        if (plugin.mBetter.contains(player)) {
+		            plugin.unsetPotion(player, 3);
+		            plugin.mBetter.remove(player);
+		            player.sendMessage(ChatColor.DARK_GRAY
+		                    +"Y"+ChatColor.GRAY+"ou have calmed your own mining enhancement at will.");
+		        }
+		        else {
+    		        plugin.mBetter.add(player);
+    		        plugin.setPotion(player, 3, 3000, 2);
+    		        player.sendMessage(ChatColor.AQUA
+    		                +"Y"+ChatColor.BLUE + "our arms suddenly feel more buff and tough!");
+    		        player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"DEX"
+    		                +ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
+    		                "2"+ChatColor.DARK_BLUE+"]");
+		        }
+		    }
+		    else {
+		        player.sendMessage(ChatColor.DARK_RED
+		                +"Y"+ChatColor.RED+"ou either need to join the Worker's Guild," +
+		                " or earn a higher rank within it.");
+		    }
 		}
-		if ((command.getName().equalsIgnoreCase("superminer"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
-				if (this.me.hasPluginPermission(this.player, "superminer")) {
-					if (this.me.mSuper.contains(this.player)) {
-						this.me.unsetPotion(this.player, 3);
-						this.me.mSuper.remove(this.player);
-						this.player.sendMessage(ChatColor.DARK_GRAY
-								+"Y"+ChatColor.GRAY+"ou have calmed your own mining enhancement at will.");
-						return true;
-					}
-					this.me.mSuper.add(this.player);
-					this.me.setPotion(this.player, 3, 4200, 3);
-					this.player.sendMessage(ChatColor.AQUA
-							+"Y"+ChatColor.BLUE + "ou feel as though you can do bicep curls for hours!");
-					this.player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"DEX"
-							+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
-							"3"+ChatColor.DARK_BLUE+"]");
-					return true;
-				} 
-				this.player.sendMessage(ChatColor.DARK_RED
-						+"Y"+ChatColor.RED+"ou either need to join the Worker's Guild," +
-						" or earn a higher rank within it.");
-			}
-			return false;
+		else if (cmdName.equalsIgnoreCase("superminer")) {
+		    if (plugin.hasPluginPermission(player, "superminer")) {
+		        if (plugin.mSuper.contains(player)) {
+		            plugin.unsetPotion(player, 3);
+		            plugin.mSuper.remove(player);
+		            player.sendMessage(ChatColor.DARK_GRAY
+		                    +"Y"+ChatColor.GRAY+"ou have calmed your own mining enhancement at will.");
+		        }
+		        else {
+		            plugin.mSuper.add(player);
+		            plugin.setPotion(player, 3, 4200, 3);
+		            player.sendMessage(ChatColor.AQUA
+		                    +"Y"+ChatColor.BLUE + "ou feel as though you can do bicep curls for hours!");
+		            player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"DEX"
+		                    +ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
+		                    "3"+ChatColor.DARK_BLUE+"]");
+		        }
+		    }
+		    else {
+		        player.sendMessage(ChatColor.DARK_RED
+		                +"Y"+ChatColor.RED+"ou either need to join the Worker's Guild," +
+		                " or earn a higher rank within it.");
+		    }
 		}
-		if ((command.getName().equalsIgnoreCase("hyperminer"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
-				if (this.me.hasPluginPermission(this.player, "hyperminer")) {
-					if (this.me.mHyper.contains(this.player)) {
-						this.me.unsetPotion(this.player, 3);
-						this.me.mHyper.remove(this.player);
-						this.player.sendMessage(ChatColor.DARK_GRAY
-								+"Y"+ChatColor.GRAY+"ou have calmed your own mining enhancement at will.");
-						return true;
-					}
-					this.me.mHyper.add(this.player);
-					this.me.setPotion(this.player, 3, 6000, 4);
-					this.player.sendMessage(ChatColor.AQUA
-							+"Y"+ChatColor.BLUE + "our biceps bulge, veins exposed with the strength to " +
-							"crush stone with your bare hands!");
-					this.player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"DEX"
-							+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
-							"4"+ChatColor.DARK_BLUE+"]");
-					return true;
-				} 
-				this.player.sendMessage(ChatColor.DARK_RED
-						+"Y"+ChatColor.RED+"ou either need to join the Worker's Guild," +
-						" or earn a higher rank within it.");
-			}
-			return false;
+		else if (cmdName.equalsIgnoreCase("hyperminer")) {
+		    if (plugin.hasPluginPermission(player, "hyperminer")) {
+		        if (plugin.mHyper.contains(player)) {
+		            plugin.unsetPotion(player, 3);
+		            plugin.mHyper.remove(player);
+		            player.sendMessage(ChatColor.DARK_GRAY
+		                    +"Y"+ChatColor.GRAY+"ou have calmed your own mining enhancement at will.");
+		        }
+		        else {
+    		        plugin.mHyper.add(player);
+    		        plugin.setPotion(player, 3, 6000, 4);
+    		        player.sendMessage(ChatColor.AQUA
+    		                +"Y"+ChatColor.BLUE + "our biceps bulge, veins exposed with the strength to " +
+    		                "crush stone with your bare hands!");
+    		        player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"DEX"
+    		                +ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
+    		                "4"+ChatColor.DARK_BLUE+"]");
+		        }
+		    } 
+		    else {
+		        player.sendMessage(ChatColor.DARK_RED
+		                +"Y"+ChatColor.RED+"ou either need to join the Worker's Guild," +
+		                " or earn a higher rank within it.");
+		    }
 		}
-		if ((command.getName().equalsIgnoreCase("godminer"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
-				if (this.me.hasPluginPermission(this.player, "godminer")) {
-					if (this.me.mGod.contains(this.player)) {
-						this.me.unsetPotion(this.player, 3);
-						this.me.mGod.remove(this.player);
-						this.player.sendMessage(ChatColor.DARK_GRAY
-								+"Y"+ChatColor.GRAY+"ou have calmed your own mining enhancement at will.");
-						return true;
-					}
-					this.me.mGod.add(this.player);
-					this.me.setPotion(this.player, 3, 9000, 5);
-					this.player.sendMessage(ChatColor.AQUA
-							+"Y"+ChatColor.BLUE + "ou feel in your arms, the power to mine so swiftly, " +
-							"that swinging your pickaxe is like waving your hand in " +
-							"the air!");
-					this.player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"DEX"
-							+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
-							"5"+ChatColor.DARK_BLUE+"]");
-					return true;
-				} 
-				this.player.sendMessage(ChatColor.DARK_RED
-						+"Y"+ChatColor.RED+"ou either need to join the Worker's Guild," +
-						" or earn a higher rank within it.");
-			}
-			return false;
+		else if (cmdName.equalsIgnoreCase("godminer")) {
+		    if (plugin.hasPluginPermission(player, "godminer")) {
+		        if (plugin.mGod.contains(player)) {
+		            plugin.unsetPotion(player, 3);
+		            plugin.mGod.remove(player);
+		            player.sendMessage(ChatColor.DARK_GRAY
+		                    +"Y"+ChatColor.GRAY+"ou have calmed your own mining enhancement at will.");
+		        }
+		        else {
+    		        plugin.mGod.add(player);
+    		        plugin.setPotion(player, 3, 9000, 5);
+    		        player.sendMessage(ChatColor.AQUA
+    		                +"Y"+ChatColor.BLUE + "ou feel in your arms, the power to mine so swiftly, " +
+    		                "that swinging your pickaxe is like waving your hand in " +
+    		                "the air!");
+    		        player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"DEX"
+    		                +ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
+    		                "5"+ChatColor.DARK_BLUE+"]");
+		        }
+		    } 
+		    else {
+    		    player.sendMessage(ChatColor.DARK_RED
+    		            +"Y"+ChatColor.RED+"ou either need to join the Worker's Guild," +
+    		            " or earn a higher rank within it.");
+		    }
 		}
-		if ((command.getName().equalsIgnoreCase("divineminer"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
-				if (this.me.hasPluginPermission(this.player, "divineminer")) {
-					if (this.me.mDivine.contains(this.player)) {
-						this.me.unsetPotion(this.player, 3);
-						this.me.mDivine.remove(this.player);
-						this.player.sendMessage(ChatColor.DARK_GRAY
-								+"Y"+ChatColor.GRAY+"ou have calmed your own mining enhancement at will.");
-						return true;
-					}
-					this.me.mDivine.add(this.player);
-					this.me.setPotion(this.player, 3, 12000, 5);
-					this.player.sendMessage(ChatColor.AQUA
-							+"Y"+ChatColor.BLUE + "our arms begin to tremble as the divine energy of " +
-							"the Minecraft Gods course through your veins!");
-					this.player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"DEX"
-							+ChatColor.GRAY+" Lvl "+ChatColor.RED+
-							"6"+ChatColor.DARK_BLUE+"]");
-					return true;
-				} 
-				this.player.sendMessage(ChatColor.DARK_RED
-						+"Y"+ChatColor.RED+"ou either need to join the Worker's Guild," +
-						" or earn a higher rank within it.");
-			}
-			return false;
+		else if (cmdName.equalsIgnoreCase("divineminer")) {
+		    if (plugin.hasPluginPermission(player, "divineminer")) {
+		        if (plugin.mDivine.contains(player)) {
+		            plugin.unsetPotion(player, 3);
+		            plugin.mDivine.remove(player);
+		            player.sendMessage(ChatColor.DARK_GRAY
+		                    +"Y"+ChatColor.GRAY+"ou have calmed your own mining enhancement at will.");
+		        }
+		        else {
+    		        plugin.mDivine.add(player);
+    		        plugin.setPotion(player, 3, 12000, 5);
+    		        player.sendMessage(ChatColor.AQUA
+    		                +"Y"+ChatColor.BLUE + "our arms begin to tremble as the divine energy of " +
+    		                "the Minecraft Gods course through your veins!");
+    		        player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"DEX"
+    		                +ChatColor.GRAY+" Lvl "+ChatColor.RED+
+    		                "6"+ChatColor.DARK_BLUE+"]");
+		        }
+		    }
+		    else {
+		        player.sendMessage(ChatColor.DARK_RED
+		                +"Y"+ChatColor.RED+"ou either need to join the Worker's Guild," +
+		                " or earn a higher rank within it.");
+		    }
 		}
-		return false;
+		
+		return true;
 	}
 }
