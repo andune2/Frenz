ed32cc0e836995f0d9e2b41871fd85faba30481d
Mark Morgan <morganm.qc@gmail.com>
2012-09-15 19:03:02 -0700

    refactor

--- src/main/java/com/live/macsephi/Thieves.java
+++ src/main/java/com/live/macsephi/Thieves.java
@@ -9,218 +9,210 @@ import org.bukkit.entity.Player;
 import com.live.macsephi.Frenz;
 
 public class Thieves implements CommandExecutor {
-	private Player player;
-	private Frenz me;
+	private Frenz plugin;
 
 	public Thieves(Frenz instance) {
-		this.me = instance;
+		this.plugin = instance;
 	}
-	public boolean onCommand(CommandSender sender, Command command,
-			String label, String[] args) {
-		if ((command.getName().equalsIgnoreCase("speed"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = null;
+        if( !(sender instanceof Player) ) {
+            sender.sendMessage("Only players may use this command");   // tell console to buzz off
+            return false;
+        }
+        else {
+            player = (Player) sender;
+        }
+        
+        final String cmdName = command.getName();
+        
+        if ((cmdName.equalsIgnoreCase("speed"))) {
+            if (plugin.hasPluginPermission(player, "speed")) {
+                if (plugin.sSpeed.contains(player)) {
+                    plugin.unsetPotion(player, 1);
+                    plugin.sSpeed.remove(player);
+                    player.sendMessage(ChatColor.DARK_GRAY
+                            +"Y"+ChatColor.GRAY+"ou have calmed your own speed enhancement at will.");
+                }
+                else {
+                    plugin.sSpeed.add(player);
+                    plugin.setPotion(player, 1, 1800, 1);
+                    player.sendMessage(ChatColor.GOLD
+                            +"Y"+ChatColor.YELLOW + "ou feel a sudden sensation in your legs, as though you can" +
+                            " run a marathon without breaking a sweat!");
+                    player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"SPD"+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
+                            "1"+ChatColor.DARK_BLUE+"]");
+                }
 
-				if (this.me.hasPluginPermission(this.player, "speed")) {
-					if (this.me.sSpeed.contains(this.player)) {
-						this.me.unsetPotion(this.player, 1);
-						this.me.sSpeed.remove(this.player);
-						this.player.sendMessage(ChatColor.DARK_GRAY
-								+"Y"+ChatColor.GRAY+"ou have calmed your own speed enhancement at will.");
-						return true;
-					}
-						this.me.sSpeed.add(this.player);
-						this.me.setPotion(this.player, 1, 1800, 1);
-						this.player.sendMessage(ChatColor.GOLD
-								+"Y"+ChatColor.YELLOW + "ou feel a sudden sensation in your legs, as though you can" +
-								" run a marathon without breaking a sweat!");
-						this.player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"SPD"+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
-								"1"+ChatColor.DARK_BLUE+"]");
-						return true;
-				
-				//	this.player.sendMessage(ChatColor.DARK_RED
-				//			+"Y"+ChatColor.RED+"ou require to possess at least 1 Diamond in your " +
-				//					"inventory to use this ability.");
-				//	return true;
-				}
-				this.player.sendMessage(ChatColor.DARK_RED
-						+"Y"+ChatColor.RED+"ou either need to join the Thieves' Guild," +
-						" or earn a higher rank within it.");
-			}
-			return false;
-		}
-		if ((command.getName().equalsIgnoreCase("hispeed"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
-				if (this.me.hasPluginPermission(this.player, "hispeed")) {
-					if (this.me.sHi.contains(this.player)) {
-						this.me.unsetPotion(this.player, 1);
-						this.me.sHi.remove(this.player);
-						this.player.sendMessage(ChatColor.DARK_GRAY
-								+"Y"+ChatColor.GRAY+"ou have calmed your own speed enhancement at will.");
-						return true;
-					}
-					this.me.sHi.add(this.player);
-					this.me.setPotion(this.player, 1, 3000, 2);
-					this.player.sendMessage(ChatColor.GOLD
-							+"Y"+ChatColor.YELLOW + "ou feel as though you can outrun olympic experts!");
-					this.player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"SPD"+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
-							"2"+ChatColor.DARK_BLUE+"]");
-					return true;
-				}
-				this.player.sendMessage(ChatColor.DARK_RED
-						+"Y"+ChatColor.RED+"ou either need to join the Thieves' Guild," +
-						" or earn a higher rank within it.");
-			}
-			return false;
-		}
-		if ((command.getName().equalsIgnoreCase("superspeed"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
-				if (this.me.hasPluginPermission(this.player, "superspeed")) {
-					if (this.me.sSuper.contains(this.player)) {
-						this.me.unsetPotion(this.player, 1);
-						this.me.sSuper.remove(this.player);
-						this.player.sendMessage(ChatColor.DARK_GRAY
-								+"Y"+ChatColor.GRAY+"ou have calmed your own speed enhancement at will.");
-						return true;
-					}
-					this.me.sSuper.add(this.player);
-					this.me.setPotion(this.player, 1, 4200, 3);
-					this.player.sendMessage(ChatColor.GOLD
-							+"Y"+ChatColor.YELLOW + "our leg muscles densify, amplifying your speed" +
-							" greater than that of even the fastest mobs!");
-					this.player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"SPD"+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
-							"3"+ChatColor.DARK_BLUE+"]");
-					return true;
-				} 
-				this.player.sendMessage(ChatColor.DARK_RED
-						+"Y"+ChatColor.RED+"ou either need to join the Thieves' Guild," +
-						" or earn a higher rank within it.");
-			}
-			return false;
-		}
-		if ((command.getName().equalsIgnoreCase("hyperspeed"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
-				if (this.me.hasPluginPermission(this.player, "hyperspeed")) {
-					if (this.me.sHyper.contains(this.player)) {
-						this.me.unsetPotion(this.player, 1);
-						this.me.sHyper.remove(this.player);
-						this.player.sendMessage(ChatColor.DARK_GRAY
-								+"Y"+ChatColor.GRAY+"ou have calmed your own speed enhancement at will.");
-						return true;
-					}
-					this.me.sHyper.add(this.player);
-					this.me.setPotion(this.player, 1, 6000, 4);
-					this.player.sendMessage(ChatColor.GOLD
-							+"A"+ChatColor.YELLOW + " massive adrenaline overtakes your entity, making time " +
-							"itself, seem to slow down all around you!");
-					this.player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"SPD"+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
-							"4"+ChatColor.DARK_BLUE+"]");
-					return true;
-				}
-				this.player.sendMessage(ChatColor.DARK_RED
-						+"Y"+ChatColor.RED+"ou either need to join the Thieves' Guild," +
-						" or earn a higher rank within it.");
-			}
-			return false;
-		}
-		if ((command.getName().equalsIgnoreCase("godspeed"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
-				if (this.me.hasPluginPermission(this.player, "godspeed")) {
-					if (this.me.sGod.contains(this.player)) {
-						this.me.unsetPotion(this.player, 1);
-						this.me.sGod.remove(this.player);
-						this.player.sendMessage(ChatColor.DARK_GRAY
-								+"Y"+ChatColor.GRAY+"ou have calmed your own speed enhancement at will.");
-						return true;
-					}
-					this.me.sGod.add(this.player);
-					this.me.setPotion(this.player, 1, 9000, 5);
-					this.player.sendMessage(ChatColor.GOLD
-							+"Y"+ChatColor.YELLOW + "ou feel godly in your reaction time, speed, and jumping ability!");
-					this.player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"SPD"+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
-							"5"+ChatColor.DARK_BLUE+"]");
-					return true;
-				}
-				this.player.sendMessage(ChatColor.DARK_RED
-						+"Y"+ChatColor.RED+"ou either need to join the Thieves' Guild," +
-						" or earn a higher rank within it.");
-			}
-			return false;
-		}
-		if ((command.getName().equalsIgnoreCase("divinespeed"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
-				if (this.me.hasPluginPermission(this.player, "divinespeed")) {
-					if (this.me.sDivine.contains(this.player)) {
-						this.me.unsetPotion(this.player, 1);
-						this.me.sDivine.remove(this.player);
-						this.player.sendMessage(ChatColor.DARK_GRAY
-								+"Y"+ChatColor.GRAY+"ou have calmed your own speed enhancement at will.");
-						return true;
-					}
-					this.me.sDivine.add(this.player);
-					this.me.setPotion(this.player, 1, 12000, 6);
-					this.player.sendMessage(ChatColor.GOLD
-							+"Y"+ChatColor.YELLOW + "our legs begin to shake as the divine energy of the " +
-							"Minecraft Gods course through your veins!");
-					this.player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"SPD"+ChatColor.GRAY+" Lvl "+ChatColor.RED+
-							"6"+ChatColor.DARK_BLUE+"]");
-					return true;
-				}
-				this.player.sendMessage(ChatColor.DARK_RED
-						+"Y"+ChatColor.RED+"ou either need to join the Thieves' Guild," +
-						" or earn a higher rank within it.");
-			}
-			return false;
-		}
-		if ((command.getName().equalsIgnoreCase("onspeed"))) {
-			if ((sender instanceof Player)) {
-				this.player = ((Player) sender);
-				if (this.me.hasPluginPermission(this.player, "own.state")) {
-					if (this.me.sDivine.contains(this.player)) {
-						this.player.sendMessage(ChatColor.DARK_BLUE
-								+"["+ChatColor.GREEN+"The state of your Divine speed is" +
-								" [ON]"+ChatColor.DARK_BLUE+"]");
+                //	player.sendMessage(ChatColor.DARK_RED
+                //			+"Y"+ChatColor.RED+"ou require to possess at least 1 Diamond in your " +
+                //					"inventory to use this ability.");
+                //	return true;
+            }
+            else {
+                player.sendMessage(ChatColor.DARK_RED
+                        +"Y"+ChatColor.RED+"ou either need to join the Thieves' Guild," +
+                        " or earn a higher rank within it.");
+            }
+        }
+        else if ((cmdName.equalsIgnoreCase("hispeed"))) {
+            if (plugin.hasPluginPermission(player, "hispeed")) {
+                if (plugin.sHi.contains(player)) {
+                    plugin.unsetPotion(player, 1);
+                    plugin.sHi.remove(player);
+                    player.sendMessage(ChatColor.DARK_GRAY
+                            +"Y"+ChatColor.GRAY+"ou have calmed your own speed enhancement at will.");
+                }
+                else {
+                    plugin.sHi.add(player);
+                    plugin.setPotion(player, 1, 3000, 2);
+                    player.sendMessage(ChatColor.GOLD
+                            +"Y"+ChatColor.YELLOW + "ou feel as though you can outrun olympic experts!");
+                    player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"SPD"+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
+                            "2"+ChatColor.DARK_BLUE+"]");
+                }
+            }
+            else {
+                player.sendMessage(ChatColor.DARK_RED
+                        +"Y"+ChatColor.RED+"ou either need to join the Thieves' Guild," +
+                        " or earn a higher rank within it.");
+            }
+        }
+        else if ((cmdName.equalsIgnoreCase("superspeed"))) {
+            if (plugin.hasPluginPermission(player, "superspeed")) {
+                if (plugin.sSuper.contains(player)) {
+                    plugin.unsetPotion(player, 1);
+                    plugin.sSuper.remove(player);
+                    player.sendMessage(ChatColor.DARK_GRAY
+                            +"Y"+ChatColor.GRAY+"ou have calmed your own speed enhancement at will.");
+                }
+                else {
+                    plugin.sSuper.add(player);
+                    plugin.setPotion(player, 1, 4200, 3);
+                    player.sendMessage(ChatColor.GOLD
+                            +"Y"+ChatColor.YELLOW + "our leg muscles densify, amplifying your speed" +
+                            " greater than that of even the fastest mobs!");
+                    player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"SPD"+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
+                            "3"+ChatColor.DARK_BLUE+"]");
+                }
+            }
+            else {
+                player.sendMessage(ChatColor.DARK_RED
+                        +"Y"+ChatColor.RED+"ou either need to join the Thieves' Guild," +
+                        " or earn a higher rank within it.");
+            }
+        }
+        else if ((cmdName.equalsIgnoreCase("hyperspeed"))) {
+            if (plugin.hasPluginPermission(player, "hyperspeed")) {
+                if (plugin.sHyper.contains(player)) {
+                    plugin.unsetPotion(player, 1);
+                    plugin.sHyper.remove(player);
+                    player.sendMessage(ChatColor.DARK_GRAY
+                            +"Y"+ChatColor.GRAY+"ou have calmed your own speed enhancement at will.");
+                }
+                else {
+                    plugin.sHyper.add(player);
+                    plugin.setPotion(player, 1, 6000, 4);
+                    player.sendMessage(ChatColor.GOLD
+                            +"A"+ChatColor.YELLOW + " massive adrenaline overtakes your entity, making time " +
+                            "itself, seem to slow down all around you!");
+                    player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"SPD"+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
+                            "4"+ChatColor.DARK_BLUE+"]");
+                }
+            }
+            else {
+                player.sendMessage(ChatColor.DARK_RED
+                        +"Y"+ChatColor.RED+"ou either need to join the Thieves' Guild," +
+                        " or earn a higher rank within it.");
+            }
+        }
+        else if ((cmdName.equalsIgnoreCase("godspeed"))) {
+            if (plugin.hasPluginPermission(player, "godspeed")) {
+                if (plugin.sGod.contains(player)) {
+                    plugin.unsetPotion(player, 1);
+                    plugin.sGod.remove(player);
+                    player.sendMessage(ChatColor.DARK_GRAY
+                            +"Y"+ChatColor.GRAY+"ou have calmed your own speed enhancement at will.");
+                }
+                else {
+                    plugin.sGod.add(player);
+                    plugin.setPotion(player, 1, 9000, 5);
+                    player.sendMessage(ChatColor.GOLD
+                            +"Y"+ChatColor.YELLOW + "ou feel godly in your reaction time, speed, and jumping ability!");
+                    player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"SPD"+ChatColor.GRAY+" Lvl "+ChatColor.GREEN+
+                            "5"+ChatColor.DARK_BLUE+"]");
+                }
+            }
+            else {
+                player.sendMessage(ChatColor.DARK_RED
+                        +"Y"+ChatColor.RED+"ou either need to join the Thieves' Guild," +
+                        " or earn a higher rank within it.");
+            }
+        }
+        else if ((cmdName.equalsIgnoreCase("divinespeed"))) {
+            if (plugin.hasPluginPermission(player, "divinespeed")) {
+                if (plugin.sDivine.contains(player)) {
+                    plugin.unsetPotion(player, 1);
+                    plugin.sDivine.remove(player);
+                    player.sendMessage(ChatColor.DARK_GRAY
+                            +"Y"+ChatColor.GRAY+"ou have calmed your own speed enhancement at will.");
+                }
+                else {
+                    plugin.sDivine.add(player);
+                    plugin.setPotion(player, 1, 12000, 6);
+                    player.sendMessage(ChatColor.GOLD
+                            +"Y"+ChatColor.YELLOW + "our legs begin to shake as the divine energy of the " +
+                            "Minecraft Gods course through your veins!");
+                    player.sendMessage(ChatColor.DARK_BLUE + "["+ChatColor.AQUA+"SPD"+ChatColor.GRAY+" Lvl "+ChatColor.RED+
+                            "6"+ChatColor.DARK_BLUE+"]");
+                }
+            }
+            else {
+                player.sendMessage(ChatColor.DARK_RED
+                        +"Y"+ChatColor.RED+"ou either need to join the Thieves' Guild," +
+                        " or earn a higher rank within it.");
+            }
+        }
+        else if ((cmdName.equalsIgnoreCase("onspeed"))) {
+            if (plugin.hasPluginPermission(player, "own.state")) {
+                if (plugin.sDivine.contains(player)) {
+                    player.sendMessage(ChatColor.DARK_BLUE
+                            +"["+ChatColor.GREEN+"The state of your Divine speed is" +
+                            " [ON]"+ChatColor.DARK_BLUE+"]");
 
-					}
-					if (this.me.sGod.contains(this.player)) {
-						this.player.sendMessage(ChatColor.DARK_BLUE
-								+"["+ChatColor.GREEN+"The state of your God speed is" +
-								" [ON]"+ChatColor.DARK_BLUE+"]");
+                }
+                if (plugin.sGod.contains(player)) {
+                    player.sendMessage(ChatColor.DARK_BLUE
+                            +"["+ChatColor.GREEN+"The state of your God speed is" +
+                            " [ON]"+ChatColor.DARK_BLUE+"]");
 
-					}
-					if (this.me.sHyper.contains(this.player)) {
-						this.player.sendMessage(ChatColor.DARK_BLUE
-								+"["+ChatColor.GREEN+"The state of your Hyper speed is" +
-								" [ON]"+ChatColor.DARK_BLUE+"]");
+                }
+                if (plugin.sHyper.contains(player)) {
+                    player.sendMessage(ChatColor.DARK_BLUE
+                            +"["+ChatColor.GREEN+"The state of your Hyper speed is" +
+                            " [ON]"+ChatColor.DARK_BLUE+"]");
 
-					}
-					if (this.me.sSuper.contains(this.player)) {
-						this.player.sendMessage(ChatColor.DARK_BLUE
-								+"["+ChatColor.GREEN+"The state of your Super speed is" +
-								" [ON]"+ChatColor.DARK_BLUE+"]");
+                }
+                if (plugin.sSuper.contains(player)) {
+                    player.sendMessage(ChatColor.DARK_BLUE
+                            +"["+ChatColor.GREEN+"The state of your Super speed is" +
+                            " [ON]"+ChatColor.DARK_BLUE+"]");
 
-					}
-					if (this.me.sHi.contains(this.player)) {
-						this.player.sendMessage(ChatColor.DARK_BLUE
-								+"["+ChatColor.GREEN+"The state of your Hi-speed is" +
-								" [ON]"+ChatColor.DARK_BLUE+"]");
+                }
+                if (plugin.sHi.contains(player)) {
+                    player.sendMessage(ChatColor.DARK_BLUE
+                            +"["+ChatColor.GREEN+"The state of your Hi-speed is" +
+                            " [ON]"+ChatColor.DARK_BLUE+"]");
 
-					}
-					if (this.me.sSpeed.contains(this.player)) {
-						this.player.sendMessage(ChatColor.DARK_BLUE
-								+"["+ChatColor.GREEN+"The state of your speed Lvl - 1 is" +
-								" [ON]"+ChatColor.DARK_BLUE+"]");
-					}
-					return true;
-				}
-				return false;
-			}		
-		}
-		return false;
+                }
+                if (plugin.sSpeed.contains(player)) {
+                    player.sendMessage(ChatColor.DARK_BLUE
+                            +"["+ChatColor.GREEN+"The state of your speed Lvl - 1 is" +
+                            " [ON]"+ChatColor.DARK_BLUE+"]");
+                }
+            }		
+        }
+
+		return true;
 	}
 }
